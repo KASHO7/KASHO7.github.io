3ec85f8710ba6ed9723997ab1265aeb3178e210c
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-11-24 16:44:46 -0500

    7B now working

--- itis3135/script/calculator.js
+++ itis3135/script/calculator.js
@@ -1,3 +1,54 @@
+window.onload = function () {
+  // Attach event listeners to buttons
+  document.getElementById("button7").onclick = function () {
+    addToInput("7");
+  };
+  document.getElementById("button8").onclick = function () {
+    addToInput("8");
+  };
+  document.getElementById("button9").onclick = function () {
+    addToInput("9");
+  };
+  document.getElementById("buttonPlus").onclick = function () {
+    addToInput("+");
+  };
+
+  document.getElementById("button4").onclick = function () {
+    addToInput("4");
+  };
+  document.getElementById("button5").onclick = function () {
+    addToInput("5");
+  };
+  document.getElementById("button6").onclick = function () {
+    addToInput("6");
+  };
+  document.getElementById("buttonMinus").onclick = function () {
+    addToInput("-");
+  };
+
+  document.getElementById("button1").onclick = function () {
+    addToInput("1");
+  };
+  document.getElementById("button2").onclick = function () {
+    addToInput("2");
+  };
+  document.getElementById("button3").onclick = function () {
+    addToInput("3");
+  };
+  document.getElementById("buttonMultiply").onclick = function () {
+    addToInput("*");
+  };
+
+  document.getElementById("buttonClear").onclick = clearInput;
+  document.getElementById("button0").onclick = function () {
+    addToInput("0");
+  };
+  document.getElementById("buttonDivide").onclick = function () {
+    addToInput("/");
+  };
+  document.getElementById("buttonEquals").onclick = calculate;
+};
+
 function addToInput(value) {
   document.getElementById("result").value += value;
 }
